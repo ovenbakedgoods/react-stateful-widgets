f867a7b54aa3fd0644c7b18a1a1eb3c596cb355a
Jabar <ovenbakedgoods88@gmail.com>
2021-06-17 11:12:19 -0500

    Updating text for moods

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -38,7 +38,7 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
-  const [mood, setMood] = useState(happyMood)
+  const [mood, setMood] = useState(initialMood)
   const makeHappy = () => {
     setMood(happyMood)
   };
